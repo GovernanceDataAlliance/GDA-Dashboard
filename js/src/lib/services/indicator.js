72a55839ed71b24b3f51f28e0f9e3afeb93a7643
Adam Mulligan <adammulligan@fastmail.fm>
2015-11-07 17:36:16 +0000

    Group indicators by name
    Adds a method to group all indicators together so that the scores are
    nested together under their indicators.

---
+++ js/src/lib/services/indicator.js
@@ -0,0 +1,27 @@
+var _ = require('lodash');
+
+var Indicators = require('../../collections/indicators.js');
+
+var IndicatorService = {
+  groupById: function(indicators) {
+    var groupedIndicators = [];
+    var ids = _.uniq(indicators.pluck('short_name'))
+
+    ids.forEach(function(id) {
+      var matchingIndicators = indicators.where({short_name: id});
+
+      var data = _.map(matchingIndicators, function(indicator) {
+        return {year: indicator.get('year'), score: indicator.get('score')};
+      });
+
+      var newIndicator = matchingIndicators[0];
+      newIndicator.set('data', data);
+
+      groupedIndicators.push(newIndicator);
+    });
+
+    return new Indicators(groupedIndicators);
+  }
+};
+
+module.exports = IndicatorService;
--- js/src/views/countries/indicator_list.js
+++ js/src/views/countries/indicator_list.js
@@ -4,18 +4,21 @@ var Backbone = require('backbone'),
 
 var IndicatorView = require('./indicator.js');
 
+var Indicators = require('../../collections/indicators.js'),
+    IndicatorService = require('../../lib/services/indicator.js');
+
 var template = Handlebars.compile(
   require('../../templates/countries/indicator_list.hbs'));
 
 var IndicatorListView = Backbone.View.extend({
   initialize: function(options) {
     options = options || {};
-    this.indicators = options.indicators;
-    this.listenTo(this.indicators, 'sync', this.render);
+
+    this.indicators = IndicatorService.groupById(options.indicators);
   },
 
   render: function() {
-    this.$el.html(template({ }));
+    this.$el.html(template({}));
 
     this.indicators.each(function(indicator) {
       var indicatorView = new IndicatorView({
