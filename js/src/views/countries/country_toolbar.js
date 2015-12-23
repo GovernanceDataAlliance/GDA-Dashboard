505b881a88c0d9577e6da47046a22f401ab909a1
Clara Linos <clara.linos@gmail.com>
2015-12-23 13:37:43 +0100

    toolbar working

--- js/src/views/countries/country_toolbar.js
+++ js/src/views/countries/country_toolbar.js
@@ -7,8 +7,6 @@ var template = Handlebars.compile(
   require('../../templates/countries/country_toolbar.hbs'));
 
 var CountryToolbarView = Backbone.View.extend({
-  
-  className: 'wrap',
 
   events: {
     'click .btn-layout-dashboard': 'gridLayoutDashboard',
@@ -20,16 +18,24 @@ var CountryToolbarView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(template({}));
-
     return this;
   },
 
   gridLayoutList: function(e) {
     $('#indicatorsContainer').removeClass('is-dashboard-layout');
+    this.activeClass(e);
   },
 
   gridLayoutDashboard: function(e) {
     $('#indicatorsContainer').addClass('is-dashboard-layout');
+    this.activeClass(e);
+  },
+
+  activeClass: function(e) {
+    $('.js--btn').removeClass('is-active');
+
+    var $target = $(e.currentTarget);
+    $target.addClass('is-active');
   }
 });
 
