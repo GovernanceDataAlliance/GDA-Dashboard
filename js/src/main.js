419a30d305626ae8a53ab4547164983e5f038a07
Clara Linos <clara.linos@gmail.com>
2016-01-19 12:07:38 +0100

    header menu working on mobile

--- js/src/main.js
+++ js/src/main.js
@@ -1,1 +1,29 @@
-var Backbone = require('backbone');
+var Backbone = require('backbone'),
+    _ = require('lodash'),
+    Handlebars = require('handlebars');
+
+var MainView = Backbone.View.extend({
+
+  el: '.l-main-container',
+
+  events: {
+    'click .js--open-menu' : 'handleMenu',
+    'click .js--close-menu' : 'handleMenu'
+  },
+  
+  initialize: function() {
+    this.cacheVars();
+  },
+
+  cacheVars: function() {
+    this.$menuWrap = $('.menus-wrapper');
+  },
+
+  handleMenu: function() {
+    this.$menuWrap.toggleClass('is-open');
+  }
+});
+
+module.exports = MainView;
+
+var main = new MainView();
