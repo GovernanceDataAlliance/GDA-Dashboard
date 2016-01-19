f251831a35b8256dfa7166a56510aed0d688e53e
Clara Linos <clara.linos@gmail.com>
2016-01-19 17:29:04 +0100

    Enrique

--- js/src/views/countries/country_list.js
+++ js/src/views/countries/country_list.js
@@ -1,13 +1,30 @@
 var Backbone = require('backbone'),
     _ = require('lodash'),
+    enquire = require('enquire.js'),
     Handlebars = require('handlebars');
 
-var template = Handlebars.compile(
-  require('../../templates/countries/country_list.hbs'));
+var template = Handlebars.compile(require('../../templates/countries/country_list.hbs'));
+var templateMb = Handlebars.compile(
+  require('../../templates/countries/country_list_mb.hbs'));
 
 var CountryListView = Backbone.View.extend({
 
   initialize: function(options) {
+
+    enquire.register("screen and (max-width:768px)", {
+      match: _.bind(function(){
+        this.mobile = true;
+        console.log('is tablet')
+      },this)
+    });
+
+    enquire.register("screen and (min-width:768px)", {
+      match: _.bind(function(){
+        this.mobile = false;
+        console.log('aint tablet')
+      },this)
+    });
+
     options = options || {};
     this.countries = options.countries;
     
