7311306e4f5576f6211c57d97bf289b597c7578f
Andrés González <andres.gonzalez@vizzuality.com>
2016-01-20 13:32:08 +0100

    Shorten descriptions

--- js/src/views/countries/indicator_list.js
+++ js/src/views/countries/indicator_list.js
@@ -12,7 +12,12 @@ var template = Handlebars.compile(require('../../templates/countries/indicators-
 var IndicatorListView = Backbone.View.extend({
 
   el: '.js--indicators',
-  
+
+  events: {
+    'click .read-more' : '_onExpandText',
+    'click .read-less' : '_onShortText'
+  },
+
   initialize: function(options) {
     options = options || {};
     this.indicators = IndicatorService.groupScoresById(options.indicators);
@@ -21,7 +26,50 @@ var IndicatorListView = Backbone.View.extend({
   render: function() {
     this.$el.html(template);
     this.renderIndicators();
+    this._shortDescription();
     return this;
+  },
+
+  _shortDescription: function() {
+    var $descriptions = $('.description'),
+      charLimit = 250,
+      textShow, textHide, readMore, readLess;
+
+    readMore = '<span class="read-more">continue reading</span>';
+    readLess = '<span class="read-less">read less</span>';
+
+    _.each($descriptions, function(d) {
+      if (d.innerText.length > charLimit) {
+
+        textShow = d.innerText.substr(0, charLimit) + '<span class="ellipsis">...</span>';
+        textHide = d.innerText.substr(charLimit, d.innerText.length);
+
+        var hiddenText = $(document.createElement('span')).html(textHide);
+        $(hiddenText).addClass('is-hidden').append(readLess);
+
+        $(d).html(textShow);
+        $(d).append(readMore);
+        $(d).append(hiddenText);
+      }
+    });
+  },
+
+  _onExpandText: function(e) {
+    var $btn = $(e.currentTarget);
+    $btn.parent().addClass('-expand');
+    $btn.next('span').removeClass('is-hidden');
+    $btn.parent().find('.ellipsis').addClass('is-hidden');
+    $btn.addClass('is-hidden');
+  },
+
+  _onShortText: function(e) {
+    var $btn = $(e.currentTarget),
+    $description = $btn.closest('.description');
+
+    $btn.parent().addClass('is-hidden');
+    $description.removeClass('-expand');
+    $description.find('.ellipsis').removeClass('is-hidden');
+    $description.find('.read-more').removeClass('is-hidden');
   },
 
   renderIndicators: function() {
