3cfab02277d984d59209c41d1d58bb58bcb35257
Clara Linos <clara.linos@gmail.com>
2016-01-22 11:57:18 +0100

    added items for new tables

--- js/src/collections/indicators.js
+++ js/src/collections/indicators.js
@@ -10,7 +10,9 @@ var Handlebars = require('handlebars');
 var SQL = Handlebars.compile(require('../templates/queries/indicators.sql.hbs')),
     SQLwithYears = Handlebars.compile(require('../templates/queries/indicators_with_years.sql.hbs'));
 
+//Do NOT forget to add here all new indexes added on CARTO
 var defaultScores = [
+  { 'short_name': 'cer', 'score': null, 'product_name': 'Citizen Engagement in Rulemaking' },
   { 'short_name': 'corruption_perceptions_index', 'score': null, 'product_name': 'Corruption Perceptions Index 2014'},
   { 'short_name': 'doing_business', 'score': null, 'product_name': 'Doing Business Report' },
   { 'short_name': 'doing_business_DTF', 'score': null, 'product_name': 'Doing Business Report' },
@@ -19,12 +21,15 @@ var defaultScores = [
   { 'short_name': 'freedom_of_the_press', 'score': null, 'product_name': 'Freedom of the Press' },
   { 'short_name': 'freedom_on_the_net', 'score': null, 'product_name': 'Freedom of the Net' },
   { 'short_name': 'global_integrity_report', 'score': null, 'product_name': 'Global Integrity Report' },
+  { 'short_name': 'illicit_financial_flows', 'score': null, 'product_name': 'Illicit Financial Flows' },
   { 'short_name': 'irm_action_plan_count_star', 'score': null, 'product_name': 'OGP IRM Number Starred Commitments' },
   { 'short_name': 'irm_action_plan_percent_star', 'score': null, 'product_name': 'OGP IRM Percentage of Starred Commitments' },
   { 'short_name': 'nations_in_transit', 'score': null, 'product_name': 'Nations in Transit' },
   { 'short_name': 'ogp_regular_consult_forum', 'score': null, 'product_name': 'Regular Forum for OGP Stakeholder Consultation' },
   { 'short_name': 'resource_governance_index', 'score': null, 'product_name': 'Resource Governance Index'  },
-  { 'short_name': 'rti_rating', 'score': null, 'product_name': 'RTI Rating' }
+  { 'short_name': 'rti_rating', 'score': null, 'product_name': 'RTI Rating' },
+  { 'short_name': 'trade_misinvoicing', 'score': null, 'product_name': 'Trade Misinvoincing Outflows' },
+  { 'short_name': 'open_budget_index', 'score': null, 'product_name': 'IBPs Open Budget Index' },
 ];
 
 var Indicators = CartoDBCollection.extend({
@@ -60,7 +65,7 @@ var Indicators = CartoDBCollection.extend({
         }
         _.extend(current, {'classColor': this._setColorsByScore(current)});
       } else {
-        rawData.rows.push(d)
+        rawData.rows.push(d);
       }
 
     }, this));
