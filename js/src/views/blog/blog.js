51097e14a99b3f8e5a31664da3920188009a80f2
Andrés González <andres.gonzalez@vizzuality.com>
2016-02-18 12:28:29 +0100

    blog cleaning

--- js/src/views/blog/blog.js
+++ js/src/views/blog/blog.js
@@ -1,8 +1,10 @@
 var $ = require('jquery'),
-    Backbone = require('backbone');
+  Backbone = require('backbone');
 
-var ShareView = require('../common/share_view.js');
-    CategorySelector = require('./category_selector.js');
+var ShareView = require('../common/share_view.js'),
+  WrapperHeaderView = require('../common/wrapper_header_view.js');
+
+var CategorySelector = require('./category_selector.js');
 
 var BlogView = Backbone.View.extend({
 
@@ -12,19 +14,19 @@ var BlogView = Backbone.View.extend({
     'click .js--share': '_share'
   },
 
-  initialize: function(options) {
-    options = options || {};
+  initialize: function() {
+    new WrapperHeaderView();
+    this.share = new ShareView();
     this._category();
   },
 
   _category: function() {
-    this.category = new CategorySelector({
+    new CategorySelector({
       el: '#categorySelector'
     });
   },
 
   _share: function() {
-    this.share = new ShareView();
     this.share.show();
     this.share._avoidScroll();
   }
