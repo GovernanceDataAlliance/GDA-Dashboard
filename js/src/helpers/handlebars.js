4e2c117fbac5e18db05c64e589d181ae6672bd33
Andrés González <andres.gonzalez@vizzuality.com>
2016-02-24 17:02:50 +0100

    yes/no answers

--- js/src/helpers/handlebars.js
+++ js/src/helpers/handlebars.js
@@ -1,11 +1,17 @@
 var Handlebars = require('handlebars');
 
 Handlebars.registerHelper('round', function(options) {
-  if (this.score % 1 != 0) {
-    return parseFloat(this.score).toFixed(2);
+
+  if (!Number.isNaN(parseFloat(this.score))) {
+    if (this.score % 1 != 0) {
+      return parseFloat(this.score).toFixed(2);
+    } else {
+      return this.score;
+    }
   } else {
     return this.score;
   }
+
 });
 
 Handlebars.registerHelper('beautifullStrign', function(options) {
