bcf77176ab48451c38171250f756ed2b1540de3f
Andrés González <andres.gonzalez@vizzuality.com>
2016-02-26 14:06:25 +0100

    Number.isNaN not supported on IE11

--- js/src/helpers/handlebars.js
+++ js/src/helpers/handlebars.js
@@ -2,7 +2,7 @@ var Handlebars = require('handlebars');
 
 Handlebars.registerHelper('round', function(options) {
 
-  if (!Number.isNaN(parseFloat(this.score))) {
+  if (!isNaN(parseFloat(this.score))) {
     if (this.score % 1 != 0) {
       return parseFloat(this.score).toFixed(2);
     } else {
