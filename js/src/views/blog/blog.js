28fa95649be5fcbedacdf91b8879645e805de0a7
Andrés González <andres.gonzalez@vizzuality.com>
2016-02-29 13:36:35 +0100

    tag cloud

--- js/src/views/blog/blog.js
+++ js/src/views/blog/blog.js
@@ -5,6 +5,8 @@ var ShareView = require('../common/share_view.js');
 
 var CategorySelector = require('./category_selector.js');
 
+var FunctionHelper = require('../../helpers/functions.js');
+
 var BlogView = Backbone.View.extend({
 
   el: '.js-blog',
@@ -15,7 +17,10 @@ var BlogView = Backbone.View.extend({
 
   initialize: function() {
     this.share = new ShareView();
+    this.FunctionHelper = FunctionHelper;
     this._category();
+
+    this.FunctionHelper.scrollTop();
   },
 
   _category: function() {
