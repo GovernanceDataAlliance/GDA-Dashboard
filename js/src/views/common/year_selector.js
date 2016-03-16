aa0dd9440563e9579df8b49c38b7154a155d03de
Andrés González <andres.gonzalez@vizzuality.com>
2016-03-16 11:23:00 +0100

    refactor enquire rules

--- js/src/views/common/year_selector.js
+++ js/src/views/common/year_selector.js
@@ -13,15 +13,40 @@ var template = Handlebars.compile(
 var CompareYearSelectors = Backbone.View.extend({
 
   events: {
-    'change select': 'getYear'
+    'change select': '_getYear'
   },
 
   initialize: function(options) {
     options = options || {};
+
     this.years = options.years;
     this.actualYear = options.actualYear;
     this.index = options.index;
+
+    this._setView();
     this.render();
+  },
+
+  _setView: function() {
+    enquire.register("screen and (max-width:640px)", {
+      match: _.bind(function() {
+        this.mobile = true;
+        this.tablet = false;
+      },this)
+    });
+
+    enquire.register("screen and (min-width:641px)", {
+      match: _.bind(function() {
+        this.mobile = false;
+        this.tablet = true;
+      },this)
+    });
+
+    enquire.register("screen and (min-width:1025px)", {
+      match: _.bind(function() {
+        this.tablet = false;
+      },this)
+    });
   },
 
   render: function() {
@@ -30,46 +55,20 @@ var CompareYearSelectors = Backbone.View.extend({
       years: this.years
     }));
 
-    this.setCurrentYear();
+    this._setCurrentYear();
 
-    Backbone.Events.trigger('year:selected', this.actualYear);
-
-    enquire.register("screen and (max-width:640px)", {
-      match: _.bind(function(){
-        this.mobile = true;
-      },this)
-    });
-
-    enquire.register("screen and (min-width:641px)", {
-      match: _.bind(function(){
-        this.mobile = false;
-      },this)
-    });
-
-    enquire.register("screen and (max-width:768px)", {
-      match: _.bind(function(){
-        this.tablet = true;
-      },this)
-    });
-
-    enquire.register("screen and (min-width:769px)", {
-      match: _.bind(function(){
-        this.tablet = false;
-      },this)
-    });
-
-    if (!this.tablet) {
+    if (!this.mobile && !this.tablet) {
       this.$('select').chosen();
     }
-
   },
 
-  setCurrentYear: function() {
-    // this.actualYear ? $('#year-'+ this.actualYear ).attr('selected', true) : $(this.$('option')[0]).attr('selected', true);
+  _setCurrentYear: function() {
     this.$el.find('select').val(this.actualYear);
+
+    Backbone.Events.trigger('year:selected', this.actualYear);
   },
 
-  getYear: function(e) {
+  _getYear: function(e) {
     var year = $(e.currentTarget).val();
     Backbone.Events.trigger('year:selected', year);
   },
