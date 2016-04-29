186f67fa4480617f7c8f2d64fcc6b6beea1b3b10
Tiago Garcia <tiagojsag@gmail.com>
2016-04-29 13:11:33 +0200

    Improve deploy script feedback

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -6,8 +6,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/ghpages-logic.sh
 
-./ghpages-logic.sh
-
+echo "Pushing to gh-pages branch"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages > /dev/null 2>&1
 
 fi
--- scripts/ghpages-logic.sh
+++ scripts/ghpages-logic.sh
@@ -3,11 +3,14 @@
 git config user.name "Travis CI"
 git config user.email "adam.mulligan@vizzuality.com"
 
+echo "Fetching existing gh-pages branch"
 git fetch
 git checkout gh-pages
 
+echo "Rebasing"
 git rebase develop
 
+echo "Installing dependencies"
 grunt dist
 
 git add -f js/*
@@ -15,5 +18,8 @@ git add -f css/main.css
 
 CHANGED=$(git diff-index --name-only HEAD --)
 if [ -n "$CHANGED" ]; then
+  echo "Commiting dependecies deltas"
   git commit -m 'Automatic Travis Build - Dependency update'
+else
+  echo "No dependency changes detected"
 fi
