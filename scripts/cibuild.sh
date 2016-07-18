1ba131f6750242d39f4f4d2537ea5b5b99d20fd2
Clara <claralinos@Claras-MacBook-Pro.local>
2016-07-18 15:58:08 +0200

    change back deploy script

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,7 +3,20 @@ set -e
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/ghpages-logic.sh
+git config user.name "Travis CI"
+git config user.email "clara.linos@vizzuality.com"
+
+git checkout -b gh-pages
+
+git rebase develop
+
+grunt dist
+
+git add -f js/*
+git add -f css/main.css
+
+git commit -m 'Automatic Travis Build'
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages > /dev/null 2>&1
 
 fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,28 @@ git fetch
 
 git checkout develop
 
+exists=`git show-ref refs/heads/gh-pages`
+if [ -n "$exists" ]; then
+  git branch -D gh-pages
+fi
+git checkout -b gh-pages
+
+git rebase develop
+
 ORIGINAL_NAME="$(git config user.name)"
 ORIGINAL_EMAIL="$(git config user.email)"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/ghpages-logic.sh
+git config user.name "Travis CI"
+git config user.email "clara.linos@vizzuality.com"
+
+grunt dist
+
+git add -f js/*
+git add -f css/main.css
+
+git commit -m 'Automatic Travis Build'
+
+git push --force --quiet origin gh-pages:gh-pages
 
 git config user.name $ORIGINAL_NAME
 git config user.email $ORIGINAL_EMAIL
